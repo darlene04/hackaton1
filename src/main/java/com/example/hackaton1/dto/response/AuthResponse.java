6bd57728f038136d36983af71846441ad6389672
Efrén Paolo Centeno Rosas <efren.centeno@utec.edu.pe>
2025-05-06 17:49:12 -0500

    Response dto

---
+++ src/main/java/com/example/hackaton1/dto/response/AuthResponse.java
@@ -0,0 +1,11 @@
+package com.example.hackaton1.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String rol;
+}
